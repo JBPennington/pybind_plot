b108dfaab8b0c4c218843bd0dcfd0fdb12e08c54
Brett Pennington <justinbrettpennington@gmail.com>
2024-07-04 10:51:02 -0700

    Init

---
+++ WORKSPACE
@@ -0,0 +1,1 @@
+workspace(name = "pybind_plot")
---
+++ plot.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <iostream>
+
+#include <pybind11/embed.h>  // py::scoped_interpreter
+#include <pybind11/stl.h>    // bindings from C++ STL containers to Python types
+
+int main() {
+    // Create an exponentially decaying sinusoidal signal as an example
+    std::vector<double> signal(1024);
+    for (size_t i = 0; i < signal.size(); ++i)
+        signal[i] = std::exp(i / -256.0) * std::cos(2 * M_PI * 8 * i / 1024.0);
+
+    // Start the Python interpreter
+    py::scoped_interpreter guard{};
+    using namespace py::literals;
+
+    // Save the necessary local variables in a Python dict
+    py::dict locals = py::dict{
+            "signal"_a = signal,
+    };
+
+    // Execute Python code, using the variables saved in `locals`
+    py::exec(R"(
+
+    import matplotlib.pyplot as plt
+
+    plt.plot(signal)
+    plt.show()
+
+    )",
+             py::globals(), locals);
+
+
+    std::cout << "Exiting..." << std::endl;
+}
